658d41bf2163440ef893d5ab5013f7d7e611af2d
Ray McDermott <ray.mcdermott@vouch.io>
2021-08-08 16:58:25 +0200

    Start script -A

--- start.sh
+++ start.sh
@@ -4,16 +4,6 @@ echo Starting REPL-REPL build
 
 set -ex
 
-echo | clojure -M:clj
-
-echo | clojure -A:clj
-
-clojure -A:figwheel
-
-clojure -M:figwheel
-
 clojure -A:cljs:figwheel:dist
 
-clojure -M:cljs:figwheel:dist
-
-clojure -M:clj:server
+clojure -A:clj:server
